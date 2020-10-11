4163ccb4cd5da2d752d957a8fe3205dcc13e813b
Xue Qianming <xueqianming200@gmail.com>
2020-10-11 22:35:03 +0800

    Add: global round 11: c

--- global-11/c.py
+++ global-11/c.py
@@ -1,29 +1,31 @@
+import os
+import sys
+from io import BytesIO, IOBase
 r, n = map(int, input().split())
 ts = [0]
 xs = [1]
 ys = [1]
 ans = [0]
 maxans = [0]
+
 def toolong(i, j):
     dis = abs(xs[i] - xs[j]) + abs(ys[i] - ys[j])
     return dis > ts[i] - ts[j]
 
 for i in range(1, n + 1):
     t, x, y = map(int, input().split())
-    print(t, x, y)
     ts.append(t)
     xs.append(x)
     ys.append(y)
-    curans = 0
-    for j in range(i - 1, max(i - 2 * r - 1, -1), -1):
+    curans = -int(1e9)
+    for j in range(i - 1, -1, -1):
+        if maxans[j] < curans:
+            break
         if not toolong(i, j):
-            curans = max(curans, ans[j] + 1)
-    print(curans)
-    if i - 2 * r - 1 >= 0:
-        curans = max(curans, maxans[i - 2 * r - 1] + 1)
-    print(curans)
+            curans = max(curans, ans[j])
+    curans += 1
     ans.append(curans)
-    maxans.append(max(maxans[-1] if maxans else 0, curans))
-print(ans)
-print(maxans)
+    maxans.append(max(maxans[-1], curans))
+# print(ans)
+# print(maxans)
 print(maxans[-1])
